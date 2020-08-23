e85b67faeb7abb91efb6d06a334143b690712257
Devin Wilson <craigvin84@yahoo.com>
2020-08-23 21:54:53 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,11 @@
 # code your #valid_move? method here
 def valid_move(board, index)
-  if # true if number is between 1 -9, false if not
-    index >= 0 && index <= 8
+if 
+  board == "" && " "
+  return true
+  
+elsif # true if number is between 1 -9, false if not
+    index >= 0 && <= 8
     return true
   end
 end

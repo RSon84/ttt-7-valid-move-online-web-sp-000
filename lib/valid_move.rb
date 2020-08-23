a6e04089cd9db8f1a27e31fda795812b37e30696
Devin Wilson <craigvin84@yahoo.com>
2020-08-23 22:02:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,13 @@
 # code your #valid_move? method here
 def valid_move(board, index)
 if
-  board == "" && " "
+  position_taken? == "" && " "
   return true
 elsif # true if number is between 1 -9, false if not
     index >= 0 && <= 8
     return true
   elsif
-    board == "X" || "Y"
+  position_taken? == "X" || "Y"
     return false
   elsif
     index < 0 && > 8
